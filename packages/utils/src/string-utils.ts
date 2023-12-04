dad90fc9d57c135f052c641d7bbee0ec2b9d0e19
Florian Hennrich <florian.hennrich@n4.de>
2023-12-04 20:06:16 +0100

    day 04

--- packages/utils/src/string-utils.ts
+++ packages/utils/src/string-utils.ts
@@ -5,3 +5,11 @@ export function isNotEmpty(str: string): boolean {
 export function isDigit(str: string): boolean {
     return !isNaN(parseInt(str));
 }
+
+export function substringAfter(str: string, char: string): string {
+    return str.substring(str.indexOf(char) + 1);
+}
+
+export function replaceWhitespace(str: string, replacer: string): string {
+    return str.replace(/\s\s+/g, replacer);
+}
