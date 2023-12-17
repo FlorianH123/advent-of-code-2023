ff5c51da2a32bf553eea4d2181451747fcf8e442
Florian Hennrich <florian.hennrich@n4.de>
2023-12-17 15:27:02 +0100

    day 07

---
+++ packages/day-07/src/main.ts
@@ -0,0 +1,17 @@
+import sourceMapSupport from 'source-map-support';
+import { readInputLines } from 'utils';
+
+import { firstPuzzle, secondPuzzle } from './day-07.js';
+
+sourceMapSupport.install();
+
+const path = new URL('../resources/puzzle-input.txt', import.meta.url);
+const input = await readInputLines(path);
+
+firstPuzzle(input)
+    .then((result) => console.log(`1. puzzle result: ${result}`))
+    .catch(console.error);
+
+secondPuzzle(input)
+    .then((result) => console.log(`2. puzzle result: ${result}`))
+    .catch(console.error);
